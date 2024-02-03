7478d21f957755943ccab5f36a9a294db8097a9f
Nasir1290 <nasirmia12901290@gmail.com>
2024-02-03 09:56:50 +0600

    it was first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 export default function App() {
   return (
     <>
-      <Header/>
+      <Header />
+      <main>
+        <div class="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+        </div>
+      </main>
     </>
-  )
+  );
 }
