4ab5100d9b0f47f3300ca4778102b5c42cef0d16
Nasir1290 <nasirmia12901290@gmail.com>
2024-02-04 18:31:31 +0600

    modified some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import MovieList from "./cine/MovieList";
 
 export default function App() {
   return (
     <>
       <Header />
       <main>
-        <div class="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
           <Sidebar />
+          <MovieList/>
         </div>
       </main>
     </>
