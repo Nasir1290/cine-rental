2fc212db88acb1a2797ef6df2f7db0dbe6ef8f5b
Nasir1290 <nasirmia12901290@gmail.com>
2024-02-04 19:23:14 +0600

    modified some files

--- src/cine/Ratings.jsx
+++ src/cine/Ratings.jsx
@@ -6,8 +6,8 @@ export default function Ratings({value}) {
     const stars = Array(value).fill(Star);
     return(
        <>
-        {stars.map((star) => (
-            <img src={star} alt="star" />
+        {stars.map((star,index) => (
+            <img key={index} width="14" height="14" src={star} alt="star" />
         ))}
        </>
     )
